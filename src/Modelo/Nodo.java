a1bedcdfbd39d48113df0dd6e68d60c967f5b598
Max Choque <maxc.m6811@gmail.com>
2016-11-03 17:00:50 -0400

    primer comit

---
+++ src/Modelo/Nodo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Max
+ */
+public class Nodo {
+    private boolean visitado;
+    private String nombre;
+    
+    
+    public Nodo(String nom){
+        nombre = nom;
+        visitado=false;
+    }
+    
+    public boolean getVisitado(){
+       return visitado;
+    }
+    public void setVisiatdo(boolean b){
+       visitado = b;
+    }
+    public String nombre(){
+    return nombre;
+    }
+    public void setNombre(String n){
+     nombre = n;
+    }
+}
